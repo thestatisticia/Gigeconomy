2e72f289a54514ba57cd057394969690ee9598cd
thestatisticia <nmushabe4@gmail.com>
2024-12-18 08:37:08 +0300

    New Project

---
+++ src/index.js
@@ -0,0 +1,17 @@
+  import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+// import {IdentityKitProvider} from "@nfid/identitykit/react"
+// import {Plug} from "@nfid/identitykit"
+// import "@nfid/identitykit/react/styles.css"
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // <IdentityKitProvider >
+    <App />
+  // </IdentityKitProvider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+
+};
+
+
+
